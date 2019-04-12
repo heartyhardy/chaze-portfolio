70059637493578540da889a5a3926ce948d69a6d
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-12 23:49:58 +0530

    Added plain Traits component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Aux from './components/hoc/Auxilary/Auxilary';
 import Splash from './components/splash/splash';
+import Traits from './components/traits/traits';
 
 const App = props => {
   return(
     <Aux styling="App">
       <Splash />
+      <Traits />
     </Aux>
   )
 }
---
+++ src/components/traits/traits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './traits.css';
+
+import Aux from '../hoc/Auxilary/Auxilary';
+
+const Traits = props => (
+    <Aux styling="traits-main">
+        <p className="traits-captions">PASSION</p>
+    </Aux>
+)
+
+export default Traits
