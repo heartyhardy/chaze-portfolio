8ba148270fc0c3a15c0a36ebf8532b10ceedbcc0
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-12 19:10:25 +0530

    Tweaked css and animation in Splash

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -5,10 +5,13 @@ import './splash.css'
 const Splash = props => (
     <Aux styling="splash-main">
         <div className="splash-chazing">
-            <p className="splash-chazing_p">༓CHAZING</p>
+            <p className="splash-chazing_p">CHAZING</p>
         </div>
         <div className="splash-dreams">
-            <p className="splash-dreams_p">DREAMS༒</p>
+            <p className="splash-dreams_p">DREAMS</p>
+        </div>
+        <div className="full-name">
+            <p className="full-name_p">Charith Rathnayake</p>
         </div>
     </Aux>
 )
