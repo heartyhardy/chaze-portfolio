0fe8b54e43e5b5faf62949b6c67ea11b4e31c943
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-13 12:06:52 +0530

    Added elements and styles to Traits

--- src/components/traits/traits.js
+++ src/components/traits/traits.js
@@ -5,7 +5,13 @@ import Aux from '../hoc/Auxilary/Auxilary';
 
 const Traits = props => (
     <Aux styling="traits-main">
-        <p className="traits-captions">PASSION</p>
+        <p className="traits-passion align-center">PASSION</p>
+        <p className="traits-creativity align-center">CREATIVITY</p>
+        <p className="traits-grit align-center">GRIT</p>
+        <p className="traits-learnability align-center">LEARNABILITY</p>
+        <p className="traits-adaptability align-center">Adaptability</p>
+        <p className="traits-leadership align-center">LEADERSHIP</p>
+        <p className="traits-empathy align-center">Empathy</p>
     </Aux>
 )
 
