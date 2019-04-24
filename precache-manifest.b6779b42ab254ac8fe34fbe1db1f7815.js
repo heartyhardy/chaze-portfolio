f04f1430f59d548b8cc76d4dc591befa5ab2f01b
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-25 00:34:20 +0530

    Updates

---
+++ precache-manifest.b6779b42ab254ac8fe34fbe1db1f7815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcf57f395a7473f1de00",
+    "url": "/chaze-portfolio/static/css/main.b2aeedd6.chunk.css"
+  },
+  {
+    "revision": "bcf57f395a7473f1de00",
+    "url": "/chaze-portfolio/static/js/main.e69ea520.chunk.js"
+  },
+  {
+    "revision": "83bb7c7a753ec990675d",
+    "url": "/chaze-portfolio/static/js/runtime~main.04fe7daa.js"
+  },
+  {
+    "revision": "4e0ed21728433aa0a09c",
+    "url": "/chaze-portfolio/static/js/2.bf658e39.chunk.js"
+  },
+  {
+    "revision": "5ed6b2d200961a7636332ff0b4003ba7",
+    "url": "/chaze-portfolio/index.html"
+  }
+];
