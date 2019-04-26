dc7a189b9c15a5a17712508756f1d3d21253d62e
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-26 23:02:27 +0530

    Changed splash screen

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -6,13 +6,13 @@ import Aux from '../hoc/Auxilary/Auxilary';
 const Splash = props => (
     <Aux styling="splash-main">
         <div className="splash-chazing">
-            <p className="splash-chazing_p">CHAZING</p>
+            <p className="splash-chazing_p">CHARITH</p>
         </div>
         <div className="splash-dreams">
-            <p className="splash-dreams_p">DREAMS</p>
+            <p className="splash-dreams_p">RATHNAYAKE</p>
         </div>
         <div className="full-name">
-            <p className="full-name_p">Charith Rathnayake</p>
+            <p className="full-name_p">Full stack web developer</p>
         </div>
         <div className="scroll-bait">
             <p className="scroll-bait_p"><i className="fas fa-angle-double-down"></i></p>
